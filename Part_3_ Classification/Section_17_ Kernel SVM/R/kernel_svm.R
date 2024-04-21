761675d5a2655bd79443b7e1ddc86e72ac479e9d
luczod <kzod5611@gmail.com>
2024-04-21 10:56:45 -0300

    feat: add kernel_svm notebook

--- Part_3_ Classification/Section_17_ Kernel SVM/R/kernel_svm.R
+++ Part_3_ Classification/Section_17_ Kernel SVM/R/kernel_svm.R
@@ -33,7 +33,9 @@ y_pred = predict(classifier, newdata = test_set[-3])
 # Making the Confusion Matrix
 cm = table(test_set[, 3], y_pred)
 
-# Visualising the Training set results
+# Visualizing the Training set results
+# region is predict by model
+# point colors is a original
 library(ElemStatLearn)
 set = training_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
@@ -46,10 +48,10 @@ plot(set[, -3],
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'dodgerblue', 'salmon'))
-points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'dodgerblue3', 'salmon3'))
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
 
-# Visualising the Test set results
+# Visualizing the Test set results
 library(ElemStatLearn)
 set = test_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
