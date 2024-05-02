5d625dfe5739926775bbdcb0995509829fe16fd3
luczod <kzod5611@gmail.com>
2024-05-02 14:29:31 -0300

    feat: add Association Rule

--- Part_5_Association Rule Learning/Section_28_Apriori/R/apriori.R
+++ Part_5_Association Rule Learning/Section_28_Apriori/R/apriori.R
@@ -11,5 +11,5 @@ itemFrequencyPlot(dataset, topN = 10)
 # Training Apriori on the dataset
 rules = apriori(data = dataset, parameter = list(support = 0.004, confidence = 0.2))
 
-# Visualising the results
+# Visualizing the results
 inspect(sort(rules, by = 'lift')[1:10])
